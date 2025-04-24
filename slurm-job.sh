6a6867232edd5f39a1fb870a8bfc70db19b1f38d
Vincent Kenbeek <vkenbeek@gmail.com>
2025-04-24 16:28:56 +0200

    td-training: add initial scripts for training on td dataset

---
+++ slurm-job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=GPU-a100
+#SBATCH --nodes=1
+#SBATCH --time=02:00:00
+#SBATCH --gres=gpu:a100:2
+
+cd /home/e12229949/TDVLM-R1
+source /home/e12229949/.bashrc
+source /home/e12229949/miniconda3/bin/activate vlm-r1
+conda init bash
+
+conda activate vlm-r1
+
+bash setup.sh
+
+pip install -U peft
+
+bash src/open-r1-multimodal/run_scripts/td-dataset.sh
---
+++ src/open-r1-multimodal/run_scripts/td-dataset.sh
@@ -0,0 +1,47 @@
+cd src/open-r1-multimodal
+
+export DEBUG_MODE="true"
+# export CUDA_VISIBLE_DEVICES=4,5,6,7
+
+RUN_NAME="Qwen2.5-VL-7B-GRPO-REC-lora-own-dataset"
+export LOG_PATH="./debug_log_$RUN_NAME.txt"
+
+# --dataset_name data_config/rec.yaml \
+# --image_root /home/e12229949/VLM-R1/ \
+
+torchrun --nproc_per_node="2" \
+    --nnodes="1" \
+    --node_rank="0" \
+    --master_addr="127.0.0.1" \
+    --master_port="12346" \
+    src/open_r1/grpo_jsonl.py \
+    --deepspeed local_scripts/zero2.json \
+    --output_dir output/$RUN_NAME \
+    --model_name_or_path Qwen/Qwen2.5-VL-7B-Instruct \
+    --dataset_name "TD-Dataset" \
+    --data_file_path /home/e12229949/VLM-R1/dataset_small/dataset.jsonl \
+    --image_folders /home/e12229949/VLM-R1/dataset_small \
+    --max_prompt_length 1024 \
+    --num_generations 2 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 2 \
+    --logging_steps 1 \
+    --bf16 \
+    --torch_dtype bfloat16 \
+    --data_seed 42 \
+    --report_to wandb \
+    --gradient_checkpointing true \
+    --attn_implementation flash_attention_2 \
+    --num_train_epochs 1 \
+    --run_name $RUN_NAME \
+    --save_steps 100 \
+    --save_only_model true \
+    --learning_rate 1e-5 \
+    --use_peft true \
+    --lora_r 64 \
+    --lora_alpha 128 \
+    --lora_dropout 0.05 \
+    --lora_task_type CAUSAL_LM \
+    --freeze_vision_modules true
+
+
